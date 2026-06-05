fea9647233cb5c385a2bfa0e3fbca78fc0698a55
jan5er <147648053+jan5er@users.noreply.github.com>
2026-06-05 20:58:55 +0200

    Auth changes

--- backend/server.js
+++ backend/server.js
@@ -14,3 +14,6 @@ mongoose.connect(process.env.MONGO_URI)
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+
+const authRoutes = require('./routes/authRoutes');
+app.use('/api/auth', authRoutes);
